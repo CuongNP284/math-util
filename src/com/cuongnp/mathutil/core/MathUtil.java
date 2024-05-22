a76c61864ae7d67fd576fbd74f7a1a13f1e6c0c3
CuongNP284 <phucuong479@gmail.com>
2024-05-22 18:04:37 +0700

    refactor the main code and green 4ever always

--- src/com/cuongnp/mathutil/core/MathUtil.java
+++ src/com/cuongnp/mathutil/core/MathUtil.java
@@ -51,7 +51,7 @@ public class MathUtil {
     //Chốt hạ: n! = n * (n-1)!
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) {
-            throw new IllegalArgumentException("Invalid argument, n must be between 0 and 20!!!");
+            throw new IllegalArgumentException("Invalid argument, n must be between 0 and 20");
         }
         if (n == 0 || n == 1) {
             return 1; //kết thúc cuộc chơi sớm nếu nhận chung đầu vào đặc biệt
